e500ce6d6801dc2c8e7876be8bc6453544b5262c
Dmitry Gruzd <donotsendhere@gmail.com>
2011-11-25 12:49:19 +0400

    first 3.0.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpRailscastsRu::Application.config.secret_token = 'f281f0b45e50bb665864e1d47df40a8e2eab737d9d0c92ddccf2b45546fa7dbbdefc409b6b88b2eb371231b27a787f3766c5348b83720ac5c9ee485640992524'
