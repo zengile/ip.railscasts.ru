029ecf40454493185abcaaa66464d6ab91d656e6
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-31 17:26:22 +0400

    capistrano & nginx & unicorn

--- autopull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $DIR
-git pull origin master
-touch tmp/restart.txt
---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :rvm_ruby_string, '1.9.3'
+set :application, "ip_show"
+set :user, "justvitalius"
+set :serv, "178.162.126.190"
+set :server_name, "ip.railscasts.ru"
+set :assets, false
+
+set :backup_db, false
+set :backup_sys, false
+
+set :repository,  "git@github.com:4freaks/ip.railscasts.ru.git"
+set :branch, "master"
+set :deploy_to, "/home/#{user}/www/#{application}"
+set :deploy_via, :remote_cache
+
+set :keep_releases, 5
+set :use_sudo, false
+
+set :scm, :git
+
+role :app, serv
+role :web, serv
+role :db, serv, :primary => true
